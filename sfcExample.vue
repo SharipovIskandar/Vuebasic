36f04343c6ea370872376bef980a135c311e9bdb
SharipovIskandar <koklok444@gmail.com>
2024-11-08 16:49:54 +0500

    first commit

---
+++ sfcExample.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{message}}</h1>
+    <button @click="changeMessage">Change Message</button>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return{
+      message: "Hello from Yamakasi"
+    };
+  },
+  methods: {
+    changeMessage(){
+      this.message = "Message changed with sfc!!!"
+    }
+  }
+};
+</script>
+<style scoped>
+ h1{
+   color: lawngreen;
+ }
+</style>
